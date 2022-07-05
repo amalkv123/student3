1237f0c3dc397ab90758a7f1272f98dd496110ce
amalkv123 <amalkvarghese2019@gmail.com>
2022-07-05 15:44:29 +0530

    sixth commit

--- sapp/urls.py
+++ sapp/urls.py
@@ -1,19 +1,63 @@
-from django.urls import path 
-from sapp import views
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 
-urlpatterns = [
+class crtcompony(models.Model):
+    componyname = models.CharField(max_length=50)
+    mailingname = models.CharField (max_length=50)
+    address = models.CharField (max_length=50)
+    state = models.CharField (max_length=50)
+    country = models.CharField (max_length=50)
+    pincode = models.CharField (max_length=10)
+    telphone = models.CharField(max_length=10)
+    mobile = models.CharField(max_length=10)
+    fax = models.CharField(max_length=10)
+    email=models.EmailField()
+    website=models.CharField(max_length=100)
+    fyearbgn=models.DateField()
+    booksbgn=models.DateField()
+    curncysymbl=models.CharField(max_length=10)
+    crncyname=models.CharField(max_length=10)
 
-    path('',views.student, name='student'),
-    path('add_students_data',views.add_students_data , name='add_students_data'),
-    path('show',views.show , name='show'),
-    path('editpage/<int:pk>',views.editpage , name="editpage"),
-    path('editstudent_data/<int:pk>',views.editstudent_data , name='editstudent_data'),
-   # path('delete/<int:pk>',views.delete , name='delete'),
-    path('delete_product/<int:pk>',views.delete_product ,  name='delete_product'),
-    path('detailview/<int:pk>',views.detailview , name='detailview')
-
-]
+class empgroup(models.Model):
+    name = models.CharField(max_length=50)
+    alias = models.CharField (max_length=50)
 
 
+class empgroup2(models.Model):
+    groupname = models.CharField(max_length=50)
+    groupalias = models.CharField (max_length=50)
+    groupunder = models.CharField (max_length=50)
+
+
+class Employee(models.Model):
+
+    name = models.CharField(max_length=225)
+    alias= models.CharField(max_length=225)
+    under= models.CharField(max_length=225)
+    date_join = models.DateField()
+    defn_sal = models.CharField(max_length=225)
+    emp_name = models.CharField(max_length=225)
+    emp_desg = models.CharField(max_length=225)
+    fnctn = models.CharField(max_length=225)
+    location = models.CharField(max_length=225)
+    gender= models.CharField(max_length=225)
+    dob = models.DateField()
+    blood = models.CharField(max_length=225)
+    parent_name =models.CharField(max_length=225)
+    spouse_name =models.CharField(max_length=225)
+    address =models.CharField(max_length=225)
+    number =models.CharField(max_length=225)
+    emailid =models.CharField(max_length=225)
+    inc_tax_no =models.CharField(max_length=225)
+    aadhar_no=models.CharField(max_length=225)
+    uan =models.CharField(max_length=225)
+    pfn =models.CharField(max_length=225)
+    pran =models.CharField(max_length=225)
+    esin =models.CharField(max_length=225)
+    bankdtls=models.CharField(max_length=225)
+   
+    
      
--- sapp/views.py
+++ sapp/views.py
@@ -1,86 +1,220 @@
-import email
-from django.shortcuts import render ,redirect
+from django.shortcuts import render,redirect
+import os
+from sapp.models import crtcompony,empgroup2,Employee
+from django.contrib.auth.models import auth,User
 from django.contrib import messages
 
-from sapp.models import student_details
 # Create your views here.
 
-# load register page
-def student(request):
-    return render(request,'student.html')
+def base(request):
+    return render(request, 'base.html')
 
-#add students data in mysql  
-def add_students_data(request):
+def changecompany(request):
+    return render(request, 'changecompany.html')
+
+def createcompony(request):
+    return render(request, 'createcompony.html')
+
+def crtecompony(request):
     if request.method=='POST':
-        sname=request.POST['student_name']
+        comname=request.POST['componyname']
+        mailingname=request.POST['mailingname']
+        address=request.POST['address']
+        state=request.POST['state']
+        country=request.POST['country']
+        pincode=request.POST['pincode']
+        telphone=request.POST['telphone']
+        mobile=request.POST['mobile']
+        fax=request.POST['fax']
         email=request.POST['email']
-        gen=request.POST['gender']
-        quli=request.POST['qulification']
-        age=request.POST['Age']
-        phn=request.POST['Phone_Number']
-        date=request.POST['Date']
-        address=request.POST['Address']
+        website=request.POST['website']
+        fyearbgn=request.POST['fyearbgn']
+        booksbgn=request.POST['booksbgn']
+        curncysymbl=request.POST['curncysymbl']
+        crncyname=request.POST['crncyname']
+        # items=request.FILES['file']
+        data=crtcompony(componyname=comname,
+                    mailingname=mailingname,
+                    address=address,
+                    state=state,
+                    country=country,
+                    pincode=pincode,
+                    telphone=telphone,
+                    mobile=mobile,
+                    fax=fax,
+                    email=email,
+                    website=website,
+                    fyearbgn=fyearbgn,
+                    booksbgn=booksbgn,
+                    curncysymbl=curncysymbl,
+                    crncyname=crncyname)
+        data.save()
+        messages.success(request,"Compony added successfully!")
+        
+        return redirect('/')
 
-        student=student_details(student_name=sname,
-                                email=email,
-                                gender=gen,
-                                qulification=quli,
-                                age=age,
-                                phone_number=phn,
-                                date=date,
-                                address=address)  
-
-        student.save()
-        print('heloo')
-        return redirect('show')
-
-
-#show students datas   
-
-def show(request):
-    datas=student_details.objects.all()
-    return render(request,'showstu.html',{'datas':datas})
-
-
-# edit 
-def editpage(request,pk):
-    datas=student_details.objects.get(id=pk)
-    return render(request,'edit.html',{'datas':datas})
+def selectcompony(request):
+    data=crtcompony.objects.all()
+    return render(request,'selectcompony.html',{'data':data})
 
 
-#edit and upadte students datas
+#payroll
 
-def editstudent_data(request ,pk):
+def emp_grp(request):
+    
+    return render(request,'employegroup2.html')     
+
+def employee(request):
+    return render(request,'employe2.html')   
+
+def payheads(request):
+    return render(request,'payheads2.html')   
+
+def attendence(request):
+    return render(request,'attendence2.html')
+
+
+
+    #edit
+
+
+def emp_grp2(request):
+    data=empgroup2.objects.all()
+    return render(request,'employegroup.html',{'p':data})  
+
+
+def emp_add(request):
     if request.method=='POST':
-        datas=student_details.objects.get(id=pk)
-        datas.student_name =request.POST.get('student_name')
-        datas.email = request.POST.get('email')
-        datas.qulification = request.POST.get('qulification')
-        datas.age = request.POST.get('Age')
-        datas.phone_number = request.POST.get('Phone_Number')
-        datas.address = request.POST.get('Address')
+        name=request.POST['name']
+        alias=request.POST['alias']
+        under=request.POST['under']
+        
+        tut=empgroup2(groupname=name,groupalias=alias,groupunder=under)
+        tut.save()
+        return redirect('emp_add2')
+    return render(request,'employegroup2.html')   
+
+def emp_add2(request):
+    emp=empgroup2.objects.all()
+    return render(request,'employegroup2.html',{'data':emp})  
+
+def emp_gredit(request,pk):
+    data=empgroup2.objects.get(id=pk)
+    return render(request,'gredit.html',{'p':data}) 
+
+def emp_gredit2(request,pk):
+    if request.method=='POST':
+        datas=empgroup2.objects.get(id=pk)
+        datas.groupname =request.POST.get('name')
+        datas.groupalias = request.POST.get('alias')
+        datas.groupunder = request.POST.get('under')
+        
 
         datas.save()
-        return redirect('show')
-      
-
-# load delete page
-#def delete(request,pk):
- #   datas=student_details.objects.get(id=pk)
-  #  return render(request,'delete.html',{'datas':datas})
-
-#delete student data
-
-def delete_product(request,pk):
-    datas=student_details.objects.get(id=pk)
-    datas.delete()
-    messages.success(request,'student data sucessfully deleted')
-    return redirect('show')
+        return redirect('emp_add2')
 
 
 
-#student detail view 
 
-def detailview(request,pk):
-    datas=student_details.objects.get(id=pk)
-    return render(request,'detailsview.html',{'datas':datas})  
+def employee2(request):
+    return render(request,'employe.html')   
+
+def payheads2(request):
+    return render(request,'payheads.html')   
+
+def attendence2(request):
+    return render(request,'attendence.html') 
+
+
+def save_employee(request):
+
+    if request.method == 'POST':
+
+        namee = request.POST['name']
+        aliass = request.POST['alias']
+        underr = request.POST['underr']
+        join = request.POST['join']
+        sal = request.POST['sal']
+        empname = request.POST['empname']
+        desig = request.POST['desig']
+        fn = request.POST['fn']
+        loc = request.POST['loc']
+        gen = request.POST['gen']
+        dob = request.POST['dob']
+        bloodd = request.POST['blood']
+        prnts = request.POST['prnts']
+        spouse = request.POST['spouse']
+        adrs = request.POST['adrs']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        taxno = request.POST['taxno']
+        aadhar = request.POST['aadhar']
+        uan = request.POST['uan']
+        pfn = request.POST['pfn']
+        pran = request.POST['pran']
+        esin = request.POST['esin']
+        bank = request.POST['bank']
+        
+        obj = Employee(
+
+            name =namee,
+            alias=aliass,
+            under=underr,
+            date_join=join,
+            defn_sal =sal,
+            emp_name = empname,
+            emp_desg=desig ,
+            fnctn = fn,
+            location =loc,
+            gender =gen,
+            dob =dob,
+            blood=bloodd,
+            parent_name =prnts,
+            spouse_name = spouse,
+            address = adrs,
+            number = phone,
+            emailid = email,
+            inc_tax_no = taxno,
+            aadhar_no = aadhar,
+            uan = uan,
+            pfn = pfn,
+            pran = pran,
+            esin = esin,
+            bankdtls = bank,
+
+
+        )
+
+        obj.save()
+        return render(request,'load_create_employee.html')
+
+                 
+
+
+
+def group(request):
+    return render(request, 'groups.html')
+
+def branch(request):
+    return render(request, 'branch.html')
+
+def ledger(request):
+    return render(request, 'ledger.html')
+
+def primary(request):
+    return render(request, 'primarycost.html')
+
+def costcat(request):
+    return render(request, 'costcat.html')
+
+def costcentr(request):
+    return render(request, 'costcentr.html')
+
+def voucher(request):
+    return render(request, 'voucher.html')
+
+def vouchpage(request):
+    return render(request, 'vouchpage.html')
+
+
+

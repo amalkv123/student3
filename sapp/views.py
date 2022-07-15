bb4a5afcdaccef11ccd5a72b6c1eca3efecf6b27
amalkv123 <amalkvarghese2019@gmail.com>
2022-07-15 16:52:28 +0530

    13 commit

--- sapp/views.py
+++ sapp/views.py
@@ -217,4 +217,378 @@ def vouchpage(request):
     return render(request, 'vouchpage.html')
 
 
+from django.shortcuts import render,redirect
+import os
+from app.models import crtcompony,empgroup2,Employee,create_VoucherModels,Create_attendence,pan
+from django.contrib.auth.models import auth,User
+from django.contrib import messages
+
+# Create your views here.
+
+def base(request):
+    return render(request, 'base.html')
+
+def changecompany(request):
+    return render(request, 'changecompany.html')
+
+def createcompony(request):
+    return render(request, 'createcompony.html')
+
+def crtecompony(request):
+    if request.method=='POST':
+        comname=request.POST['componyname']
+        mailingname=request.POST['mailingname']
+        address=request.POST['address']
+        state=request.POST['state']
+        country=request.POST['country']
+        pincode=request.POST['pincode']
+        telphone=request.POST['telphone']
+        mobile=request.POST['mobile']
+        fax=request.POST['fax']
+        email=request.POST['email']
+        website=request.POST['website']
+        fyearbgn=request.POST['fyearbgn']
+        booksbgn=request.POST['booksbgn']
+        curncysymbl=request.POST['curncysymbl']
+        crncyname=request.POST['crncyname']
+        # items=request.FILES['file']
+        data=crtcompony(componyname=comname,
+                    mailingname=mailingname,
+                    address=address,
+                    state=state,
+                    country=country,
+                    pincode=pincode,
+                    telphone=telphone,
+                    mobile=mobile,
+                    fax=fax,
+                    email=email,
+                    website=website,
+                    fyearbgn=fyearbgn,
+                    booksbgn=booksbgn,
+                    curncysymbl=curncysymbl,
+                    crncyname=crncyname)
+        data.save()
+        messages.success(request,"Compony added successfully!")
+        
+        return redirect('/')
+
+def selectcompony(request):
+    data=crtcompony.objects.all()
+    return render(request,'selectcompony.html',{'data':data})
+
+
+#payroll
+
+def emp_grp(request):
+    
+    return render(request,'employegroup2.html')     
+
+def employee(request):
+    return render(request,'employe2.html')   
+
+def payheads(request):
+    return render(request,'payheads2.html') 
+
+def gst(request):
+    return render(request,'gst.html')  
+
+def pan2(request):
+    return render(request,'pan.html')  
+
+def attendence(request):
+    data=Create_attendence.objects.all()
+    return render(request,'attendence2.html',{'p':data})
+
+def unit2(request):
+    return render(request,'unit2.html')
+
+def payroll(request):
+    return render(request,'payroll.html')
+
+
+
+    #edit
+
+
+def emp_grp2(request):
+    data=empgroup2.objects.all()
+    return render(request,'employegroup.html',{'p':data})  
+
+
+def emp_add(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        alias=request.POST['alias']
+        under=request.POST['under']
+        
+        tut=empgroup2(groupname=name,groupalias=alias,groupunder=under)
+        tut.save()
+        return redirect('emp_add2')
+    return render(request,'employegroup2.html')   
+
+def emp_add2(request):
+    emp=empgroup2.objects.all()
+    return render(request,'employegroup2.html',{'data':emp})  
+
+def emp_gredit(request,pk):
+    data=empgroup2.objects.get(id=pk)
+    data2=empgroup2.objects.all()
+    context={'p':data,
+    'p2':data2}
+    return render(request,'gredit.html',context) 
+
+def emp_gredit2(request,pk):
+    if request.method=='POST':
+        datas=empgroup2.objects.get(id=pk)
+        datas.groupname =request.POST.get('name')
+        datas.groupalias = request.POST.get('alias')
+        datas.groupunder = request.POST.get('under')
+        
+
+        datas.save()
+        return redirect('emp_add2')
+
+
+
+
+def employee2(request):
+    return render(request,'employe.html')   
+
+def payheads2(request):
+    return render(request,'payheads.html')   
+
+def attendence2(request):
+    data=Create_attendence.objects.all()
+    return render(request,'attendence.html',{'p':data})
+
+def attendence3(request):
+    if request.method == 'POST':
+        name=request.POST['name']
+        alias=request.POST['alias']
+        under=request.POST['under']
+        type=request.POST['type']
+        
+        std=Create_attendence(
+            name =name,
+            alias=alias,
+            under=under,
+            type=type,
+           )
+        std.save()
+        messages.success(request,'successfully Added !!!')
+        return redirect('attendence')
+
+
+def attendence_edit(request,pk):
+    data=Create_attendence.objects.get(id=pk)
+    data2=Create_attendence.objects.all()
+    context={'p':data,
+    'p2':data2}
+    return render(request,'attendence_edit.html',context) 
+
+def attendence_edit2(request,pk):
+    if request.method == 'POST':
+        data=Create_attendence.objects.get(id=pk)
+        data.name=request.POST.get('name')
+        data.alias=request.POST.get('alias')
+        data.under=request.POST.get('under')
+        data.type=request.POST.get('type')
+        data.save()
+        return redirect('attendence')
+
+
+
+def save_employee(request):
+
+    if request.method == 'POST':
+
+        namee = request.POST['name']
+        aliass = request.POST['alias']
+        underr = request.POST['underr']
+        join = request.POST['join']
+        sal = request.POST['sal']
+        empname = request.POST['empname']
+        desig = request.POST['desig']
+        fn = request.POST['fn']
+        loc = request.POST['loc']
+        gen = request.POST['gen']
+        dob = request.POST['dob']
+        bloodd = request.POST['blood']
+        prnts = request.POST['prnts']
+        spouse = request.POST['spouse']
+        adrs = request.POST['adrs']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        taxno = request.POST['taxno']
+        aadhar = request.POST['aadhar']
+        uan = request.POST['uan']
+        pfn = request.POST['pfn']
+        pran = request.POST['pran']
+        esin = request.POST['esin']
+        bank = request.POST['bank']
+        
+        obj = Employee(
+
+            name =namee,
+            alias=aliass,
+            under=underr,
+            date_join=join,
+            defn_sal =sal,
+            emp_name = empname,
+            emp_desg=desig ,
+            fnctn = fn,
+            location =loc,
+            gender =gen,
+            dob =dob,
+            blood=bloodd,
+            parent_name =prnts,
+            spouse_name = spouse,
+            address = adrs,
+            number = phone,
+            emailid = email,
+            inc_tax_no = taxno,
+            aadhar_no = aadhar,
+            uan = uan,
+            pfn = pfn,
+            pran = pran,
+            esin = esin,
+            bankdtls = bank,
+
+
+        )
+
+        obj.save()
+        return render(request,'load_create_employee.html')
+
+
+
+def add_voucher(request):
+    if request.method == 'POST':
+        Vname = request.POST['name']
+        alias = request.POST['alias']
+        vtype = request.POST['type']
+        abbre = request.POST['abber']
+        activ_vou_typ = request.POST['active']  
+        meth_vou_num = request.POST['numbering']
+        useadv = request.POST.get('config', False)
+        prvtdp = request.POST.get('prevent', False)
+       
+        use_effct_date = request.POST['effect']  
+        allow_zero_trans = request.POST['trans']  
+        allow_naration_in_vou = request.POST['narr']  
+        optional = request.POST['optical'] 
+        provide_narr = request.POST['ledg']  
+        print = request.POST['print']  
+        
+        std = create_VoucherModels(voucher_name=Vname ,
+            alias=alias,
+            voucher_type=vtype,
+            abbreviation=abbre,
+            active_this_voucher_type=activ_vou_typ,
+            method_voucher_numbering=meth_vou_num,
+            use_effective_date=use_effct_date,
+            use_adv_conf = useadv,
+            prvnt_duplictes =prvtdp,
+            allow_zero_value_trns=allow_zero_trans,
+            allow_naration_in_voucher=allow_naration_in_vou,
+            make_optional=optional,
+            provide_naration=provide_narr,
+            print_voucher=print,
+
+        )
+        std.save()
+        return redirect('add_voucher2')
+
+    return render(request, 'payroll.html')  
+
+
+def add_voucher2(request):
+    emp=create_VoucherModels.objects.all()
+    return render(request,'payroll2.html',{'data':emp}) 
+
+def add_voucher3(request):
+    emp=create_VoucherModels.objects.all()
+    return render(request,'payroll.html',{'data':emp}) 
+
+def add_voucher_edit(request,pk):
+    emp=create_VoucherModels.objects.get(id=pk)
+    data2=create_VoucherModels.objects.all()
+    context={'p':emp,
+    'data':data2}
+    return render(request,'payrolledit.html',context) 
+
+def add_voucher_edit2(request,pk):
+    emp=create_VoucherModels.objects.get(id=pk)
+    emp.voucher_name=request.POST.get('name')
+    emp.alias=request.POST.get('alias')
+    emp.voucher_type=request.POST.get('type')
+    emp.abbreviation=request.POST.get('abber')
+    emp.active_this_voucher_type=request.POST.get('active')
+    emp.method_voucher_numbering=request.POST.get('numbering')
+    emp.use_adv_conf=request.POST.get('config', False)
+    emp.prvnt_duplictes=request.POST.get('prevent', False)
+    emp.use_effective_date=request.POST.get('effect')
+    emp.allow_zero_value_trns=request.POST.get('trans')
+    emp.allow_naration_in_voucher=request.POST.get('narr')
+    emp.make_optional=request.POST.get('optical')
+    emp.provide_naration=request.POST.get('ledg')
+    emp.print_voucher=request.POST.get('print')
+    emp.save()
+    return redirect('add_voucher2')
+
+
+
+
+def unit(request):
+    return render(request, 'unit.html')
+
+
+def panadd(request):
+    if request.method == 'POST':
+        tax2=request.POST['tax']
+        no2=request.POST['no']
+
+        std = pan(tax3 = tax2,
+        no = no2)
+        std.save()
+        return redirect('pan2')
+    return render(request, 'pan.html')
+
+
+
+
+
+
+
+
+                 
+
+
+
+def group(request):
+    return render(request, 'groups.html')
+
+def branch(request):
+    return render(request, 'branch.html')
+
+def ledger(request):
+    return render(request, 'ledger.html')
+
+def primary(request):
+    return render(request, 'primarycost.html')
+
+def costcat(request):
+    return render(request, 'costcat.html')
+
+def costcentr(request):
+    return render(request, 'costcentr.html')
+
+def voucher(request):
+    return render(request, 'voucher.html')
+
+def vouchpage(request):
+    return render(request, 'vouchpage.html')
+
+
+
 

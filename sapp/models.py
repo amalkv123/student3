901a43228613e8a0442128b0d995a184d7a02429
amalkv123 <amalkvarghese2019@gmail.com>
2022-07-15 16:50:55 +0530

    12th commit

--- sapp/models.py
+++ sapp/models.py
@@ -14,3 +14,106 @@ class student_details(models.Model):
     date=models.DateField()
     address=models.CharField(max_length=255)
 
+
+
+class crtcompony(models.Model):
+    componyname = models.CharField(max_length=50)
+    mailingname = models.CharField (max_length=50)
+    address = models.CharField (max_length=50)
+    state = models.CharField (max_length=50)
+    country = models.CharField (max_length=50)
+    pincode = models.CharField (max_length=10)
+    telphone = models.CharField(max_length=10)
+    mobile = models.CharField(max_length=10)
+    fax = models.CharField(max_length=10)
+    email=models.EmailField()
+    website=models.CharField(max_length=100)
+    fyearbgn=models.DateField()
+    booksbgn=models.DateField()
+    curncysymbl=models.CharField(max_length=10)
+    crncyname=models.CharField(max_length=10)
+
+class empgroup(models.Model):
+    name = models.CharField(max_length=50)
+    alias = models.CharField (max_length=50)
+
+
+class empgroup2(models.Model):
+    groupname = models.CharField(max_length=50)
+    groupalias = models.CharField (max_length=50)
+    groupunder = models.CharField (max_length=50)
+
+
+class Employee(models.Model):
+
+    name = models.CharField(max_length=225)
+    alias= models.CharField(max_length=225)
+    under= models.CharField(max_length=225)
+    date_join = models.DateField()
+    defn_sal = models.CharField(max_length=225)
+    emp_name = models.CharField(max_length=225)
+    emp_desg = models.CharField(max_length=225)
+    fnctn = models.CharField(max_length=225)
+    location = models.CharField(max_length=225)
+    gender= models.CharField(max_length=225)
+    dob = models.DateField()
+    blood = models.CharField(max_length=225)
+    parent_name =models.CharField(max_length=225)
+    spouse_name =models.CharField(max_length=225)
+    address =models.CharField(max_length=225)
+    number =models.CharField(max_length=225)
+    emailid =models.CharField(max_length=225)
+    inc_tax_no =models.CharField(max_length=225)
+    aadhar_no=models.CharField(max_length=225)
+    uan =models.CharField(max_length=225)
+    pfn =models.CharField(max_length=225)
+    pran =models.CharField(max_length=225)
+    esin =models.CharField(max_length=225)
+    bankdtls=models.CharField(max_length=225)
+
+class create_VoucherModels(models.Model):
+    voucher_name = models.CharField(max_length=225)
+    alias = models.CharField(max_length=225)
+    voucher_type = models.CharField(max_length=225)
+    abbreviation = models.CharField(max_length=225)
+    active_this_voucher_type =  models.CharField(max_length=225)
+    method_voucher_numbering = models.CharField(max_length=225)
+    use_adv_conf = models.CharField(max_length=225,blank=True)
+    prvnt_duplictes = models.CharField(max_length=225,default="Null",blank=True)
+    use_effective_date =  models.CharField(max_length=225,default="Null")
+    allow_zero_value_trns =  models.CharField(max_length=225)
+    allow_naration_in_voucher =  models.CharField(max_length=225)
+    make_optional =  models.CharField(max_length=225)
+    provide_naration =  models.CharField(max_length=225)
+    print_voucher = models.CharField(max_length=225)
+
+
+class Create_attendence(models.Model):
+    name =models.CharField(max_length=225)
+    alias=models.CharField(max_length=225)
+    under=models.CharField(max_length=225)
+    type =models.CharField(max_length=225)
+
+
+class pan(models.Model):
+    tax3 =models.IntegerField(max_length=225)
+    no =models.IntegerField(max_length=225)
+
+
+class create_payhead(models.Model):
+    name=models.CharField(max_length=225)
+    alias=models.CharField(max_length=225)
+    pay_type=models.CharField(max_length=225)
+    income_type=models.CharField(max_length=225)
+    under=models.CharField(max_length=225)
+    affect_net=models.CharField(max_length=225)
+    payslip=models.CharField(max_length=225)
+    calculation_of_gratuity=models.CharField(max_length=225)
+    cal_type=models.CharField(max_length=225)
+    calculation_period=models.CharField(max_length=225)
+    leave_withpay=models.CharField(max_length=225)
+    leave_with_out_pay=models.CharField(max_length=225)
+    production_type=models.CharField(max_length=225)
+    opening_balance=models.CharField(max_length=225)
+   
+    
